e14317e3e3d3d4e40a2c1d8aace1cd25ba288af5
Mark Olson <41911657+mholson@users.noreply.github.com>
2024-12-24 08:21:19 +0100

    Adding custom.el and two yasnippet defns

---
+++ doom/.config/doom/custom.el
@@ -0,0 +1,16 @@
+;;; ../../GitHub/mho-dotfiles/doom/.config/doom/custom.el -*- lexical-binding: t; -*-
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
