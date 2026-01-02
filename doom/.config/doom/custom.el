85252d667d3b8d116a8c326921e6d7b8dcf097bb
Mark Olson <41911657+mholson@users.noreply.github.com>
2026-01-02 11:54:42 +0100

    safe themes update

--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -7,7 +7,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
+   '("dfcd2b13f10da4e5e26eb1281611e43a134d4400b06661445e7cbb183c47d2ec"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
      "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
